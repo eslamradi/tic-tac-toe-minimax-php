71fda8bb0b569c85aa97b52ef6e6742eb025c39e
eslamradi <eslamradii@gmail.com>
2021-10-26 10:28:27 +0200

    fix some logic errors validation

--- src/Validator.php
+++ src/Validator.php
@@ -1,18 +1,25 @@
-<?php 
+<?php
 
 namespace TicTacToe;
 
-class Validator {
+use TicTacToe\Board;
 
-    public function isValidCellInput($input) {
-        if (in_array($input , ['11','12','13','21','22','23','31','32','33'])) {
-            return true;
+class Validator
+{
+
+    public function isValidCellInput($input, Board $board)
+    {
+        if (in_array($input, ['11', '12', '13', '21', '22', '23', '31', '32', '33'])) {
+            if ($board->state->getCellValue((int)$input[0], (int)$input[1]) === '_') {
+                return true;
+            }
         }
         return false;
     }
 
-    public function isAnswerYes($input) {
-        if (in_array(strtolower($input) , ['y', 'yes'])) {
+    public function isAnswerYes($input)
+    {
+        if (in_array(strtolower($input), ['y', 'yes'])) {
             return true;
         }
         return false;
